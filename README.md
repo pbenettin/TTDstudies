a889420f6a21c7cbc511e3ce018ac0866edef929
Paolo Benettin <paolo.benettin@epfl.ch>
2022-06-22 17:15:05 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,32 @@
+Analysis of TTD studies in real-world catchments
+================
+
+This repository explores a list of scientific papers on water transit
+time distributions (TTD) in real-world systems. The list currently
+includes 83 studies from 53 distinct sites published between 2007 and
+2022.
+
+A paper is included if it is based on tracer data and if it makes use of
+post-2006 approaches
+
+## Display the data
+
+The analyses can be run through the R Markdown script
+“data\_analysis.Rmd”
+
+The ouputs include:
+
+  - A table with all [the studies]()
+
+  - A table with all the [study sites]()
+
+  - An interactive html [map]() that identifies the location of the
+    sites
+
+## How to contribute to this table
+
+Please feel free to suggest additional studies that may have been left
+out by mistake. You can directly send a table entry formatted as in the
+tables `data/table_papers.csv` and `data/sites_location_classified.csv`
+(we can take care of adding the climate classification, if this is
+problematic).
---
+++ data_import.R
@@ -0,0 +1,46 @@
+# LOAD PACKAGES AND PROCESS THE TABLES
+
+# load all packages
+library(readr)
+library(stringr)
+library(dplyr)
+library(tidyr)
+library(glue)
+library(tibble)
+
+# load all the data into one tibble
+T <- read_csv(file.path("data","table_papers.csv")) %>%
+  mutate(doi = str_glue("https://doi.org/{doi}")) %>%
+  full_join(read_csv(file.path("data","sites_location_classified.csv")),by = c("site","system")) %>%
+  left_join(read_csv(file.path("data","climate_classification.csv")),by = c("climate_id"))
+
+# remove geographic climate from fully-controlled experiments, if not already done
+T <- T %>%
+mutate(climate_code = ifelse(site %in% c(
+ "LEO hillslopes (East, West), Landscape Evolution Observatory, Biosphere 2",
+ "miniLEO, Landscape Evolution Observatory, Biosphere 2"
+ ),NA,climate_code))
+
+
+# modify the table for improved html display
+Tdisplay <- T %>%
+  mutate(tracer = str_replace_all(tracer,"H-2",'<sup>2</sup>H')) %>% #improve display
+  mutate(tracer = str_replace_all(tracer,"O-18",'<sup>18</sup>O')) %>% #improve display
+  mutate(tracer = str_replace_all(tracer,"H-3",'<sup>3</sup>H')) %>% #improve display
+  mutate(tracer = str_replace_all(tracer,"NO3",'NO<sup>3</sup>')) %>% #improve display
+  mutate(study = glue("<a href='{doi}' target='_blank'>{author_year}</a>")) %>%
+  mutate(year = str_match(author_year,"\\d{4}")) %>%
+  relocate(study, year) %>%
+  arrange(year)
+
+# create a neat list of study sites only
+Tsites <- Tdisplay %>%
+  group_by(site, system) %>%
+  mutate(n_studies = n()) %>%
+  arrange(desc(year)) %>% #for each catchment, order studies by year
+  mutate(studies = str_c(unique(study), collapse = "<br>")) %>% #merge a list of studies per site
+  ungroup() %>%
+  distinct(site,system, .keep_all = TRUE) %>%
+  select(site, system, size_km2, country, lat_WGS84, long_WGS84,
+         climate_main, climate_code, climate_description, n_studies, studies) %>%
+  arrange(site)
